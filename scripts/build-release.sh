86f3781b9b56fe5225c2245147994c7833ea83cb
Ingmar Stein <IngmarStein@users.noreply.github.com>
2025-02-24 09:52:32 +0100

    Include libpixlet in release tarball

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -20,23 +20,28 @@ do
 
 	echo "Building ${RELEASE_PLATFORM}_${RELEASE_ARCH}"
 
+	PIXLET=pixlet
+	LIBPIXLET=libpixlet.so
 	if [[ $ARCH == "linux-arm64"  ]]; then
 		echo "linux-arm64"
-		CC=aarch64-linux-gnu-gcc CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -lsharpyuv -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
-		CC=aarch64-linux-gnu-gcc CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -lsharpyuv -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -tags lib -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/libpixlet.so -buildmode=c-shared library/library.go
+		CC=aarch64-linux-gnu-gcc CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -lsharpyuv -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${PIXLET}" tidbyt.dev/pixlet
+		CC=aarch64-linux-gnu-gcc CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -lsharpyuv -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -tags lib -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${LIBPIXLET}" -buildmode=c-shared library/library.go
 	elif [[ $ARCH == "linux-amd64"  ]]; then
 		echo "linux-amd64"
-		CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s '-extldflags=-static -lsharpyuv' -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
-		CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -tags lib -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/libpixlet.so -buildmode=c-shared library/library.go
+		CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s '-extldflags=-static -lsharpyuv' -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${PIXLET}" tidbyt.dev/pixlet
+		CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -tags lib -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${LIBPIXLET}" -buildmode=c-shared library/library.go
 	elif [[ $ARCH == "windows-amd64"  ]]; then
 		echo "windows-amd64"
-		go build -ldflags="-s '-extldflags=-static -lsharpyuv' -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -tags timetzdata -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet.exe tidbyt.dev/pixlet
-		go build -ldflags="-s '-extldflags=-static -lsharpyuv'" -tags lib -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet.dll library/library.go
+		PIXLET=pixlet.exe
+		LIBPIXLET=pixlet.dll
+		go build -ldflags="-s '-extldflags=-static -lsharpyuv' -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -tags timetzdata -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${PIXLET} tidbyt.dev/pixlet
+		go build -ldflags="-s '-extldflags=-static -lsharpyuv'" -tags lib -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${LIBPIXLET}" library/library.go
 	else
 		echo "other"
-		CGO_CFLAGS="-I/tmp/${LIBWEBP_VERSION}/${ARCH}/include" CGO_LDFLAGS="-L/tmp/${LIBWEBP_VERSION}/${ARCH}/lib" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
+		CGO_CFLAGS="-I/tmp/${LIBWEBP_VERSION}/${ARCH}/include" CGO_LDFLAGS="-L/tmp/${LIBWEBP_VERSION}/${ARCH}/lib" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${PIXLET}" tidbyt.dev/pixlet
+		CGO_CFLAGS="-I/tmp/${LIBWEBP_VERSION}/${ARCH}/include" CGO_LDFLAGS="-L/tmp/${LIBWEBP_VERSION}/${ARCH}/lib" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -tags lib -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/libpixlet.so library/library.go
 	fi
 
-	echo "Built ./build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet successfully"
-	tar -cvz -C "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}" -f "build/pixlet_${PIXLET_VERSION}_${RELEASE_PLATFORM}_${RELEASE_ARCH}.tar.gz" pixlet
+	echo "Built ./build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/${PIXLET} successfully"
+	tar -cvz -C "build/${RELEASE_PLATFORM}_${RELEASE_ARCH}" -f "build/pixlet_${PIXLET_VERSION}_${RELEASE_PLATFORM}_${RELEASE_ARCH}.tar.gz" ${PIXLET} ${LIBPIXLET}
 done
