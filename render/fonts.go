34e744d04e37c3928222f21a884337ff290cdfd7
Gabe Cook <gabe565@gmail.com>
2025-10-16 00:26:44 -0500

    perf(render): embed fonts using `go:embed` (#203)

--- render/fonts.go
+++ render/fonts.go
@@ -1,25 +1,33 @@
 package render
 
-//go:generate go run gen/embedfonts.go
-
 import (
-	"encoding/base64"
+	"errors"
 	"fmt"
+	"os"
+	"path"
+	"strings"
 	"sync"
 
 	"github.com/zachomedia/go-bdf"
 	"golang.org/x/image/font"
+	"tidbyt.dev/pixlet/fonts"
 )
 
 var fontCache = map[string]font.Face{}
 var fontMutex = &sync.Mutex{}
 
-func GetFontList() []string {
-	fontNames := []string{}
-	for key := range fontDataRaw {
-		fontNames = append(fontNames, key)
+func GetFontList() ([]string, error) {
+	entries, err := fonts.Fonts.ReadDir(".")
+	if err != nil {
+		return nil, err
 	}
-	return fontNames
+
+	fontNames := make([]string, 0, len(entries))
+	for _, e := range entries {
+		fontNames = append(fontNames, trimExt(e.Name()))
+	}
+
+	return fontNames, nil
 }
 
 func GetFont(name string) (font.Face, error) {
@@ -30,21 +38,23 @@ func GetFont(name string) (font.Face, error) {
 		return font, nil
 	}
 
-	dataB64, ok := fontDataRaw[name]
-	if !ok {
-		return nil, fmt.Errorf("unknown font '%s'", name)
-	}
-
-	data, err := base64.StdEncoding.DecodeString(dataB64)
+	data, err := fonts.Fonts.ReadFile(name + ".bdf")
 	if err != nil {
-		return nil, fmt.Errorf("decoding font '%s': %w", name, err)
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("unknown font %q", name)
+		}
+		return nil, fmt.Errorf("reading font %q: %w", name, err)
 	}
 
 	f, err := bdf.Parse(data)
 	if err != nil {
-		return nil, fmt.Errorf("parsing font '%s': %w", name, err)
+		return nil, fmt.Errorf("parsing font %q: %w", name, err)
 	}
 
 	fontCache[name] = f.NewFace()
 	return fontCache[name], nil
+}
+
+func trimExt(filename string) string {
+	return strings.TrimSuffix(filename, path.Ext(filename))
 }
