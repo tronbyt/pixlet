5c9c53832fb9cad0885b603e8cd6f830faf43c9a
Gabe Cook <gabe565@gmail.com>
2025-10-31 03:16:35 -0500

    feat: add list-fonts subcommand

---
+++ cmd/community/listfonts.go
@@ -0,0 +1,106 @@
+package community
+
+import (
+	"cmp"
+	"fmt"
+	"log/slog"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+	"github.com/zachomedia/go-bdf"
+	"tidbyt.dev/pixlet/fonts"
+)
+
+var ListFontsCmd = &cobra.Command{
+	Use:     "list-fonts",
+	Short:   "List available fonts.",
+	Example: `  pixlet community list-fonts`,
+	Long:    `This command lists all fonts supported by this Pixlet version.`,
+	RunE:    listFonts,
+}
+
+type fontEntry struct {
+	name       string
+	advanceMin int
+	advanceMax int
+	height     int
+	ascent     int
+	descent    int
+}
+
+func listFonts(cmd *cobra.Command, _ []string) error {
+	dir, err := fonts.FS.ReadDir(".")
+	if err != nil {
+		return fmt.Errorf("could not read fonts: %w", err)
+	}
+
+	entries := make([]fontEntry, 0, len(dir))
+	for _, entry := range dir {
+		if strings.HasSuffix(entry.Name(), fonts.Ext) {
+			name := strings.TrimSuffix(entry.Name(), fonts.Ext)
+
+			b, err := fonts.GetBytes(name)
+			if err != nil {
+				slog.Error("could not read font", "name", name, "err", err)
+				continue
+			}
+
+			f, err := bdf.Parse(b)
+			if err != nil {
+				slog.Error("could not parse font", "name", name, "err", err)
+				continue
+			}
+
+			var advanceMin, advanceMax int
+			if len(f.Characters) != 0 {
+				advanceMin = math.MaxInt
+				advanceMax = math.MinInt
+				for _, c := range f.Characters {
+					advanceMin = min(c.Advance[0], advanceMin)
+					advanceMax = max(c.Advance[0], advanceMax)
+				}
+			}
+
+			entries = append(entries, fontEntry{
+				name:       name,
+				advanceMin: advanceMin,
+				advanceMax: advanceMax,
+				height:     f.Ascent + f.Descent,
+				ascent:     f.Ascent,
+				descent:    f.Descent,
+			})
+		}
+	}
+
+	slices.SortFunc(entries, func(a, b fontEntry) int {
+		return cmp.Or(
+			cmp.Compare(a.height, b.height),
+			cmp.Compare(a.name, b.name),
+		)
+	})
+
+	w := tabwriter.NewWriter(cmd.OutOrStdout(), 0, 0, 3, ' ', 0)
+	if _, err := w.Write([]byte("NAME\tADVANCE\tHEIGHT\tASCENT\tDESCENT\n")); err != nil {
+		return err
+	}
+
+	for _, e := range entries {
+		advance := strconv.Itoa(e.advanceMin)
+		if e.advanceMin != e.advanceMax {
+			advance += "-" + strconv.Itoa(e.advanceMax)
+		}
+
+		if _, err := fmt.Fprintf(
+			w, "%s\t%s\t%d\t%d\t%d\n",
+			e.name, advance, e.height, e.ascent, e.descent,
+		); err != nil {
+			return err
+		}
+	}
+
+	return w.Flush()
+}
