5718689ff9fc3b09afb442ee537e18313195e3c8
Gabe Cook <git@gabe565.com>
2026-03-25 23:14:45 -0500

    perf(file): preallocate buffer size

--- runtime/modules/file/file.go
+++ runtime/modules/file/file.go
@@ -1,13 +1,13 @@
 package file
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"io/fs"
+	"unsafe"
 
 	"github.com/mitchellh/hashstructure/v2"
 	"go.starlark.net/starlark"
-	"go.starlark.net/starlarkstruct"
 )
 
 type File struct {
@@ -15,39 +15,35 @@ type File struct {
 	Path string
 }
 
-func (f *File) readall(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	var mode starlark.String
-	if err := starlark.UnpackArgs("readall", args, kwargs, "mode?", &mode); err != nil {
+func (f *File) readall(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var modeParam starlark.String
+
+	if err := starlark.UnpackArgs(
+		b.Name(), args, kwargs,
+		"mode?", &modeParam,
+	); err != nil {
 		return nil, err
 	}
 
-	r, err := f.reader(string(mode))
+	mode, err := ParseMode(string(modeParam))
 	if err != nil {
 		return nil, err
 	}
-	defer func() { _ = r.Close() }()
 
-	return r.read(thread, nil, nil, nil)
-}
+	bs, err := fs.ReadFile(f.FS, f.Path)
+	if err != nil {
+		return nil, err
+	}
 
-func (f *File) reader(mode string) (*Reader, error) {
-	var binaryMode bool
 	switch mode {
-	case "", "r", "rt":
-		binaryMode = false
-
-	case "rb":
-		binaryMode = true
-
+	case ModeReadText:
+		str := unsafe.String(unsafe.SliceData(bs), len(bs))
+		return starlark.String(str), nil
+	case ModeReadBinary:
+		return starlark.Bytes(bs), nil
 	default:
-		return nil, fmt.Errorf("unsupported mode: %s", mode)
+		return nil, ErrUnsupportedMode
 	}
-
-	fl, err := f.FS.Open(f.Path)
-	if err != nil {
-		return nil, err
-	}
-	return &Reader{fl, binaryMode}, nil
 }
 
 func (f *File) AttrNames() []string {
@@ -58,10 +54,8 @@ func (f *File) Attr(name string) (starlark.Value, error) {
 	switch name {
 	case "path":
 		return starlark.String(f.Path), nil
-
 	case "readall":
 		return starlark.NewBuiltin("readall", f.readall), nil
-
 	default:
 		return nil, nil
 	}
@@ -77,58 +71,23 @@ func (f *File) Hash() (uint32, error) {
 	return uint32(sum), err
 }
 
-type Reader struct {
-	io.ReadCloser
+type Mode uint8
 
-	binaryMode bool
-}
+const (
+	ModeNone Mode = iota
+	ModeReadText
+	ModeReadBinary
+)
 
-func (r Reader) Struct() *starlarkstruct.Struct {
-	return starlarkstruct.FromStringDict(starlark.String("Reader"), starlark.StringDict{
-		"read": starlark.NewBuiltin("read", r.read),
-		"close": starlark.NewBuiltin("close", func(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-			return nil, r.Close()
-		}),
-	})
-}
+var ErrUnsupportedMode = errors.New("unsupported mode")
 
-// read reads the contents of the file. The Starlark signature is:
-//
-//	read(size=-1) -> bytes
-func (r Reader) read(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	starlarkSize := starlark.MakeInt(-1)
-	if err := starlark.UnpackArgs("read", args, kwargs, "size?", &starlarkSize); err != nil {
-		return nil, err
-	}
-
-	var size int
-	if err := starlark.AsInt(starlarkSize, &size); err != nil {
-		return nil, fmt.Errorf("size is not an int")
-	}
-
-	returnType := func(buf []byte) starlark.Value {
-		if r.binaryMode {
-			return starlark.Bytes(buf)
-		} else {
-			return starlark.String(buf)
-		}
-	}
-
-	if size < 0 {
-		// read and return all bytes
-		buf, err := io.ReadAll(r)
-		if err != nil {
-			return nil, err
-		}
-
-		return returnType(buf), nil
-	} else {
-		// read and return size bytes
-		buf := make([]byte, size)
-		_, err := r.Read(buf)
-		if err != nil {
-			return nil, err
-		}
-		return returnType(buf), nil
+func ParseMode(mode string) (Mode, error) {
+	switch mode {
+	case "", "r", "rt":
+		return ModeReadText, nil
+	case "rb":
+		return ModeReadBinary, nil
+	default:
+		return ModeNone, fmt.Errorf("%w: %s", ErrUnsupportedMode, mode)
 	}
 }
